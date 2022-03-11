fa639fe8653557fb16af6c0cbf435d2e068746e6
3110680 <3110680@TE-GDA-D-D26F04>
2022-03-11 15:14:19 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Random r = new Random();
+int liczba = r.Next(1, 10);
+int liczba2;
+bool win = false;
+Console.WriteLine("Konsola wygenerowała liczbę od 1 do 10,zgadnij jaka to liczba)");
+liczba2 = Convert.ToInt32(Console.ReadLine());
+ while (liczba2 != liczba)
+{
+    if (liczba2 < liczba)
+    {
+        Console.WriteLine("Twoja liczba jest zbyt mała,spróbuj ponownie");
+        liczba2 = Convert.ToInt32(Console.ReadLine());
+    }
+    else
+    {
+        if (liczba2 > liczba)
+        {
+            Console.WriteLine("Twoja liczba jest zbyt wysoka,spróbuj ponownie");
+            liczba2 = Convert.ToInt32(Console.ReadLine());
+        }
+        if (liczba2 == liczba)
+
+
+        {
+            win = true;
+            Console.WriteLine("Gratulacje! Udało ci się zgadnąć poprawną liczbę!");
+            Console.WriteLine("Twoja liczba to" + liczba);
+            break;
+        }
+    }
+}
+
+
+
